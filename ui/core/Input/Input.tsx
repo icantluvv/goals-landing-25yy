b9e94f25b9925e9dd5df5ac3712de58bdb925299
Sergey <spanteleev670@gmail.com>
2025-09-05 00:10:39 +0300

    test commit

--- ui/core/Input/Input.tsx
+++ ui/core/Input/Input.tsx
@@ -1,42 +1,43 @@
 "use client"
 
 import { IMaskInput } from "react-imask"
+import { ChangeEvent } from "react"
 
 type InputProps = {
-  placeholder: string
-  className?: string
-  type: string
-  mask?: string
-  value?: string
-  onChange?: (e: any) => void
+    placeholder: string
+    className?: string
+    type: string
+    mask?: string
+    value?: string
+    onChange?: (e: ChangeEvent<HTMLInputElement> | { target: { value: string } }) => void
 }
 
 const Input = ({ placeholder, className, type, mask, value = "", onChange }: InputProps) => {
-  const inputClassNames = `w-full px-1.5 py-3 border-b placeholder:text-gray-500 placeholder:font-poppins border-PrimaryBlack/25 focus:outline-none ${className}`
+    const inputClassNames = `w-full px-1.5 py-3 border-b placeholder:text-gray-500 placeholder:font-poppins border-PrimaryBlack/25 focus:outline-none ${className}`
 
-  return (
-    <div className="relative w-full">
-      {mask ? (
-        <IMaskInput
-          mask={mask}
-          value={value}
-          onAccept={(val: string) => {
-            onChange?.({ target: { value: val } })
-          }}
-          className={inputClassNames}
-          placeholder={placeholder}
-        />
-      ) : (
-        <input
-          type={type}
-          value={value}
-          onChange={onChange}
-          className={inputClassNames}
-          placeholder={placeholder}
-        />
-      )}
-    </div>
-  )
+    return (
+        <div className="relative w-full">
+            {mask ? (
+                <IMaskInput
+                    mask={mask}
+                    value={value}
+                    onAccept={(val: string) => {
+                        onChange?.({ target: { value: val } })
+                    }}
+                    className={inputClassNames}
+                    placeholder={placeholder}
+                />
+            ) : (
+                <input
+                    type={type}
+                    value={value}
+                    onChange={onChange}
+                    className={inputClassNames}
+                    placeholder={placeholder}
+                />
+            )}
+        </div>
+    )
 }
 
 export default Input
