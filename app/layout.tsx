a7b72df6be697e2978678e127a3929bfb3f76159
PanteleevSergey <s.panteleev@webpractik.ru>
2025-10-20 13:25:11 +0300

    feat(scrollbar): добавил стили для скроллбара

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,23 +4,23 @@ import DefaultLayout from "@/ui/shared/default-layout/default-layout"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
-  icons: {
-    icon: [
-      { url: "/favicon.ico", type: "image/x-icon" },
-      { url: "/favicon.svg", type: "image/svg+xml" }
-    ]
-  }
+    icons: {
+        icon: [
+            { url: "/favicon.ico", type: "image/x-icon" },
+            { url: "/favicon.svg", type: "image/svg+xml" }
+        ]
+    }
 }
 export default function RootLayout({
-  children
+    children
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="ru" className={`${inter.variable} ${montserrat.variable}`}>
-      <body>
-        <DefaultLayout>{children}</DefaultLayout>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="ru" className={`${inter.variable} ${montserrat.variable} `}>
+            <body>
+                <DefaultLayout>{children}</DefaultLayout>
+            </body>
+        </html>
+    )
 }
